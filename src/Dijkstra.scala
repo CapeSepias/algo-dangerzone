ab558106a3dca4face943d870e16472774adfc68
Konstantin Perikov <konstantin.perikov@kohls.com>
2013-08-06 15:40:31 +0400

    Add skeleton for PA#5

---
+++ src/Dijkstra.scala
@@ -0,0 +1,30 @@
+/**
+ * @author kperikov
+ *
+ *         Library of different Dijsktra implementation
+ */
+object Dijkstra {
+
+
+  /**
+   * Slow Dijkstra implementation O(n * m)
+   */
+  def solveDijkstraSlow(): Unit = {
+
+  }
+
+  /**
+   * Dijkstra algorithm, with fast data structure (heap). Use Scala standard library
+   */
+  def solveDijkstraStandardHeap(): Unit = {
+
+  }
+
+  /**
+   * Dijkstra algorithm, with fast data structure (heap). Write own heap implementation
+   */
+  def solveDijkstraSelfWrittenHeap(): Unit = {
+
+  }
+
+}
