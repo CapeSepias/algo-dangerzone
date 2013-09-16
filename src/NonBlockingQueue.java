62f63ef1d6538cf20739ddbe2dd978589a3e27b4
Konstantin Perikov <konstantin.perikov@kohls.com>
2013-09-16 11:35:03 +0400

    Fix NonBlockingQueue contract

--- src/NonBlockingQueue.java
+++ src/NonBlockingQueue.java
@@ -2,7 +2,7 @@
  * @author kperikov
  *         <p/>
  *         Same as
- * @see BlockingQueue but, if queue doesn't contain any element - be in waiting state
+ * @see BlockingQueue but, if queue doesn't contain any element - return null
  */
 public class NonBlockingQueue {
 }
