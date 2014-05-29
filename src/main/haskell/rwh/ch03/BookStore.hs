762db8ed0dba645a295d8200d6daf6deba73f629
Konstantin Perikov <kperikov@griddynamics.com>
2014-05-29 12:21:04 +0400

    add some more

--- src/main/haskell/rwh/ch03/BookStore.hs
+++ src/main/haskell/rwh/ch03/BookStore.hs
@@ -1,3 +1,8 @@
 -- file: ch03/BookStore.hs
 data BookInfo = Book Int String [String]
                 deriving (Show)
+
+type CustomerID = Int
+type ReviewBody = String
+
+data BetterReview = BetterReview BookInfo CustomerID ReviewBody
