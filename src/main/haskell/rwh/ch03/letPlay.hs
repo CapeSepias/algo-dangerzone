aa4babfaa2553e09124e464b59bf9f7918319643
Mysterion <pericov.ko@gmail.com>
2014-06-03 21:43:10 +0400

    add mroe stuff

---
+++ src/main/haskell/rwh/ch03/letPlay.hs
@@ -0,0 +1,2 @@
+foo = let a = 3 in let b = a + 2 in b
+
--- src/main/haskell/rwh/ch03/mySecond.hs
+++ src/main/haskell/rwh/ch03/mySecond.hs
@@ -4,6 +4,15 @@ mySecond xs = if null (tail xs)
               then error "list too short"
               else head (tail xs)
 
--- main = putStrLn $ mySecond ["Hello!"]
 
-main =  putStrLn $ mySecond ["123 ", "World!"]
+safeSecond :: [a] -> Maybe a
+
+safeSecond [] = Nothing
+safeSecond xs = if null (tail xs)
+                then Nothing
+                else Just (head (tail xs))
+
+tidySecond :: [a] -> Maybe a
+
+tidySecond (_:x:_) = Just x
+tidySecond  _      = Nothing
