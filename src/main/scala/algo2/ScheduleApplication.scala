074d72626e4423f2061775e14f306207c7890ccf
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2015-03-18 17:30:06 +0100

    Create ScheduleApplication.scala

---
+++ src/main/scala/algo2/ScheduleApplication.scala
@@ -0,0 +1,13 @@
+import scala.math._
+
+import java.io.{InputStreamReader, BufferedReader}
+import java.util.{Random, StringTokenizer}
+
+/**
+ * Solution for PA#1
+ */
+object ScheduleApplication {
+
+  // TODO
+
+}
