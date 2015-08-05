7c6691c084384968aa2b6874836008f47e12021a
Konstantin Perikov <kperikov@griddynamics.com>
2015-08-05 21:48:19 +0200

    start wk 2

--- src/main/scala/bio1/wk1/A.scala
+++ src/main/scala/bio1/wk1/A.scala
@@ -1,18 +1,8 @@
-package bio1.wk1
-
-import scala.io.StdIn._
+package main.scala.bio1.wk1
 
 object A {
 
   def main(args: Array[String]) = {
-    val text = readLine
-    val pattern = readLine
-    var ans = 0
-    (0 to text.length - pattern.length).foreach(i => {
-      if (text.substring(i, i + pattern.length).equalsIgnoreCase(pattern)) {
-        ans += 1
-      }
-    })
-    println(ans)
+    
   }
 }
