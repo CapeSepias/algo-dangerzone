3d93e82447240bf1c00861ad0bac370ed1a760d4
Konstantin Perikov <kperikov@griddynamics.com>
2015-08-06 18:10:50 +0200

    add correct A sol

--- src/main/scala/bio1/wk2/A.scala
+++ src/main/scala/bio1/wk2/A.scala
@@ -8,13 +8,21 @@ object A {
     val genome = readLine
     var countG = 0
     var countC = 0
-    print("0 ")
+    val skew = new Array[Int](genome.length + 1)
+    var ind = 1
     genome.foreach(x => {
       if (x == 'G')
         countG += 1
       else if (x == 'C')
         countC += 1
-      print((countG - countC) + " ")
+      skew(ind) = countG - countC
+      ind += 1
     })
+    val min = skew.reduceLeft(_ min _)
+    for (i <- 0 until skew.length) {
+      if (skew(i) == min) {
+        print(i + " ")
+      }
+    }
   }
 }
